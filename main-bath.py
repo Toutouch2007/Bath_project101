c21f59bdd758bc677fcc801d4a77b250b8093bc9
Lenny Yahav <u9786846@bsmch.net>
2026-08-19 20:14:04 +0300

    functions

--- main-bath.py
+++ main-bath.py
@@ -1,14 +1,21 @@
 import time
-Time = time.gmtime()
-Dates = ["20/8", "23/8","24/8","25/8","26/8","27/8","30/8","31/8","1/9","2/9","3/9","6/9","7/9","8/9","9/9","10/9","13/9","14/9","15/9","16/9","17/9"]
-Total = len(Dates)
-DayCollected = False
-for i in range(Total):
-    if f"{Time.tm_mday}/{Time.tm_mon}" == Dates[i]:
-        print(f"You Have {Total -  i -1} Baths Left")
-        DayCollected = True
-        break
-if not DayCollected:
-    print(f"You Have {Total} Baths Left")
-input("Press Enter To Continue...")
- 
+def bath():
+    Time = time.gmtime()
+    Dates = ["20/8", "23/8","24/8","25/8","26/8","27/8","30/8","31/8","1/9","2/9","3/9","6/9","7/9","8/9","9/9","10/9","13/9","14/9","15/9","16/9","17/9"]
+    Total = len(Dates)
+    DayCollected = False
+    for i in range(Total):
+        if f"{Time.tm_mday}/{Time.tm_mon}" == Dates[i]:
+            print(f"You Have {Total -  i -1} Baths Left")
+            DayCollected = True
+            break
+    if not DayCollected:
+        print(f"You Have {Total} Baths Left")
+    input("Press Enter To Continue...")
+    
+
+def main():
+    bath
+
+if __name__ == '__main__':
+    main()
